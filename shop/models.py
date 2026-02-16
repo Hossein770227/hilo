ab9eb9751ac2835bf961f119aec08a9ddd42b3e1
hossein <hossein.h.a770227@gmail.com>
2026-02-16 13:48:09 +0330

    add category and product model for shop app

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(_("name"), max_length=100)
+    slug = models.SlugField(max_length=150, unique=True,allow_unicode=True, blank=True)
+    class Meta:
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
+
+    def __str__(self):
+            return self.name
+    
+
+
+class Product(models.Model):
+    title = models.CharField(_("title"), max_length=100)
+    slug = models.SlugField(_("slug"), unique=True)
+    category = models.ForeignKey(Category, verbose_name=_("category"), on_delete=models.CASCADE, related_name='products')
+    price_main = models.PositiveIntegerField(_("price"))
+    price_with_discount = models.PositiveIntegerField(_("price with discount"), blank=True, null=True)
+    amount = models.IntegerField(_("amount"), validators=MinValueValidator(1))
+    date_time_added = models.DateTimeField(_("date time added"), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("product")
+        verbose_name_plural = _("products")
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.slug])
+    
+
+    def clean(self):
+       super().clean()
+       if self.price_with_discount and self.price_with_discount >= self.price_main:
+            raise ValidationError(_("discount price must be less than original price"))
+
+    def __str__(self):
+        return self.title
